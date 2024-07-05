3783e209e5be236b60e7c018803f26e8e27563ed
Krish-Ramoliya <krishramoliya434@gmail.com>
2024-07-05 17:22:15 +0530

    add code for weatherapp

--- RoboSpeaker/Speaker.py
+++ RoboSpeaker/Speaker.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     print("Welcome to RoboSpeaker 1.1 Created by Krish")
     while True:
         x= input("Enter what you want me to speak: ")
-        if x == 'q' :
+        if x == 'q':
             break
         speak = win.Dispatch("SAPI.SpVoice")
         speak.Speak(x)
---
+++ WeatherApp/Weather.py
@@ -0,0 +1,11 @@
+import json
+
+import requests
+
+city = input("Enter the name of the city\n ")
+url=f"http://api.weatherapi.com/v1/current.json?key=5b1e069dd6be423487c114056240507&q={city}"
+r= requests.get(url)
+
+wdic= json.loads(r.text)
+print(wdic["current"]["temp_c"])
+# print(r.text)
