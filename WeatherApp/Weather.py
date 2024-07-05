fc046e9d3bd8f0a69a0b9e0b1b865d28053624b6
Krish-Ramoliya <krishramoliya434@gmail.com>
2024-07-05 17:54:10 +0530

    Add Imageresizer

--- WeatherApp/Weather.py
+++ WeatherApp/Weather.py
@@ -1,11 +1,10 @@
 import json
-
 import requests
 
 city = input("Enter the name of the city\n ")
 url=f"http://api.weatherapi.com/v1/current.json?key=5b1e069dd6be423487c114056240507&q={city}"
 r= requests.get(url)
+# print(r.text)
 
 wdic= json.loads(r.text)
 print(wdic["current"]["temp_c"])
-# print(r.text)
